e364d4c5d407aa9a5ed6615b393ee48354a57837
blanceyes <70919513+blanceyes@users.noreply.github.com>
2020-09-07 20:30:42 -0500

    Added file-based input for adjectives and nouns

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
-var adjectives = ['Retarded', 'Pozzed', 'Gamer', 'Racist', 'Pedophile', 'Rapist', 'Incel', 'African', 'Trans'];
-var nouns = ['Nick', 'Adam', 'Stav', 'Molestrios', 'Ian', 'Dasha', 'Day Laborer', 'Tony Soprano', 'Jeffrey Epstein'];
+var adjectives = new Array;
+readFile(adjectives, 'res/adjectives.txt');
+var nouns = new Array;
+readFile(nouns, 'res/nouns.txt');
 
 function generateCharacter(e) {
-    document.getElementById('adj').innerHTML = adjectives[Math.floor(Math.random()*adjectives.length)];
-    document.getElementById('noun').innerHTML = nouns[Math.floor(Math.random()*nouns.length)];
-    
+	document.getElementById('adj').innerHTML = adjectives[Math.floor(Math.random()*adjectives.length)];
+	document.getElementById('noun').innerHTML = nouns[Math.floor(Math.random()*nouns.length)];
 }
 
-//addEventListener('click', generateCharacter);
+function readFile(array, fileName) {
+	$.get(fileName, function(data) {
+		newArray = data.split('\n');
+		newArray.forEach(element => array.push(element));
+	})
+}
+
+$(document).ready(function() {
+	console.log('jquery loaded');
+});
+
 document.getElementById('gen').addEventListener('click', generateCharacter);

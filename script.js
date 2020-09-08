b790f069683796a418ac16bc93ffadf3744035bf
blanceyes <70919513+blanceyes@users.noreply.github.com>
2020-09-08 10:06:10 -0500

    Mobile fix attempt 2

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-var adjectives = new Array;
-readFile(adjectives, 'res/adjectives.txt');
+var descriptors = new Array;
+readFile(descriptors, 'res/descriptors.txt');
 var nouns = new Array;
 readFile(nouns, 'res/nouns.txt');
 
 function generateCharacter(e) {
-	document.getElementById('adj').innerHTML = adjectives[Math.floor(Math.random()*adjectives.length)];
+	document.getElementById('des').innerHTML = descriptors[Math.floor(Math.random()*descriptors.length)];
 	document.getElementById('noun').innerHTML = nouns[Math.floor(Math.random()*nouns.length)];
 }
 
@@ -19,4 +19,5 @@ $(document).ready(function() {
 	console.log('jquery loaded');
 });
 
+//document.getElementById('gen').addEventListener('click', generateCharacter);
 document.getElementById('gen').addEventListener('click', generateCharacter);
